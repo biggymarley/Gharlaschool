ea7cbe9c2f5efcd0fdf8cf53dfee67161e49f1d2
afaragi <afaragi@zobristinc.com>
2024-12-25 02:58:08 +0100

    .gharla skool

--- src/skate.jsx
+++ src/skate.jsx
@@ -447,7 +447,7 @@ const SkateSchool = () => {
       <footer className="bg-gray-900 py-8 px-4">
         <div className="max-w-6xl mx-auto text-center">
           <p className="text-gray-400">
-            © 2024 Radical Skate School. All rights reserved.
+            © 2024 Gharla Skate School.
           </p>
         </div>
       </footer>
