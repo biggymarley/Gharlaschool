3690ad121c3312321398add3de7cf7acf6ccd3d7
afaragi <afaragi@zobristinc.com>
2024-12-30 13:04:30 +0100

    ui improvements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
 export const Footer = () => (
-    <footer className="bg-gray-900 py-8 px-4">
-      <div className="max-w-6xl mx-auto text-center">
-        <p className="text-gray-400">© 2024 Gharla Skate School.</p>
+  <footer className="bg-gray-800 py-8 px-4">
+    <div className="max-w-6xl mx-auto text-center">
+      <p className="text-gray-400">© 2024 Gharla Skate School.</p>
+      <div className="flex justify-center gap-2 text-gray-400 mt-4 ">
+        <span className="text-sm">Created with</span>
+        <img src="/assets/skateboarding.png" className="w-5 h-5" />
+        <a href="https://www.instagram.com/bbigggyy/" className="text-sm" target="_blank">by biggy</a>
       </div>
-    </footer>
-  );
+    </div>
+  </footer>
+);
